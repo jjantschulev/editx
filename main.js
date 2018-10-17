761162a6f6facd63c274461d22534ca3dc9a36dc
Jordan Jantschulev <j.jantschulev@gmail.com>
2018-10-17 19:51:27 +1100

    Added Copy Paste lines and Added notification feature

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const engine = require('./engine.js');
+const Notification = require('./notification.js');
 
 const sideBarWidth = 5;
 const navHeight = 2;
@@ -17,9 +18,8 @@ codeExtensions = codeExtensions.split(' ');
 var fileExtension = filename.indexOf('.') != -1 ? filename.slice(filename.indexOf('.') + 1) : "";
 if (codeExtensions.indexOf(fileExtension) != -1) syntax = fileExtension;
 
-var modified = false;
 var notSavedWarning = false;
-var notSavedWarningText = "File is not saved. Changes will be lost. Press ESC to continue";
+var modified = false;
 var scrollY = 0,
     scrollX = 0;
 
@@ -31,35 +31,32 @@ var wantedCursorPosX = 0;
 typeableChars = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+[]{}|;':\",.<>/?`~"
 typeableChars = typeableChars.split('');
 
+var notification;
 
 var file = fs.readFileSync(filename);
 var lines = file.toString().split("\n");
 
+var copyBuffer = "";
+
 function setup() {
+    notification = new Notification.Notification(engine);
     engine.render();
 }
 
 const draw = function () {
     // HEADER
-    if (notSavedWarning) {
-        engine.fillBackground('red');
-        engine.fillForeground('red');
-        engine.drawLine(0, 0, 1, 0, engine.width, engine.BOX);
-        engine.fillForeground('black');
-        engine.drawText(Math.floor(engine.width / 2) - Math.floor(notSavedWarningText.length / 2), 0, notSavedWarningText);
+    engine.fillBackground('yellow')
+    engine.fillForeground('yellow');
+    engine.drawLine(0, 0, 1, 0, engine.width, engine.BOX);
+    engine.fillForeground('black');
+    engine.drawText(2, 0, "ESC to close");
+    engine.drawText(Math.floor(engine.width / 2) - Math.floor(filename.length / 2), 0, filename);
+    if (modified) {
+        engine.drawText(engine.width - 10, 0, "modified");
     } else {
-        engine.fillBackground('yellow')
-        engine.fillForeground('yellow');
-        engine.drawLine(0, 0, 1, 0, engine.width, engine.BOX);
-        engine.fillForeground('black');
-        engine.drawText(2, 0, "ESC to close");
-        engine.drawText(Math.floor(engine.width / 2) - Math.floor(filename.length / 2), 0, filename);
-        if (modified) {
-            engine.drawText(engine.width - 10, 0, "modified");
-        } else {
-            engine.drawText(engine.width - 7, 0, "saved");
-        }
+        engine.drawText(engine.width - 7, 0, "saved");
     }
+
     engine.noBg();
 
     // File Rendering
@@ -74,6 +71,9 @@ const draw = function () {
             renderLine(line, y);
         }
     }
+
+    notification.show(engine);
+
     engine.setCursor(cursorPosX + sideBarWidth, cursorPosY - scrollY + navHeight);
     // engine.drawPoint(cursorPosX + sideBarWidth, cursorPosY - scrollY + navHeight, engine.BOX);
 }
@@ -82,10 +82,10 @@ const draw = function () {
 function keyPressed(key) {
     // Cursor Control:
     if (key.name == 'left') {
-        moveCursorX(1);
+        moveCursorX(-1);
     }
     if (key.name == 'right') {
-        moveCursorX(-1);
+        moveCursorX(1);
     }
     if (key.name == 'up') {
         moveCursorY(-1);
@@ -95,9 +95,6 @@ function keyPressed(key) {
     }
 
     // Special Keys
-    if (key.ctrl && key.name == 's') {
-        saveFile();
-    }
     if (key.name == 'tab' && !key.ctrl) {
         var remainder = cursorPosX % 4;
         remainder = 4 - remainder == 0 ? 4 : 4 - remainder;
@@ -116,13 +113,53 @@ function keyPressed(key) {
         returnKey();
     }
     if (key.name == 'escape') {
-        if (!modified || notSavedWarning) {
-            engine.exit();
-        } else {
-            // User has not saved. Create popup.
-            notSavedWarning = true;
-            setTimeout(() => { notSavedWarning = false; engine.render() }, 3000);
+        exit();
+    }
+    if (key.ctrl) {
+        if (key.ctrl && key.name == 's') {
+            saveFile();
         }
+        if (key.name == 'c') {
+            copyBuffer = lines[cursorPosY];
+            notification.notify("Line copied", 1000);
+        }
+        if (key.name == 'v') {
+            lines.splice(cursorPosY, 0, copyBuffer);
+            moveCursorY(1);
+            modified = true;
+        }
+        if (key.name == 'x') {
+            copyBuffer = lines[cursorPosY];
+            lines.splice(cursorPosY, 1);
+            if (lines.length == 0) {
+                lines.push("");
+            }
+            moveCursorY(-1);
+            notification.notify("Line cut", 1000);
+            modified = true;
+        }
+        if (key.name == 'q') {
+            if (cursorPosY > 0) {
+                var temp = lines[cursorPosY - 1];
+                lines[cursorPosY - 1] = lines[cursorPosY];
+                lines[cursorPosY] = temp;
+                moveCursorY(-1);
+                modified = true;
+            }
+        }
+        if (key.name == 'a') {
+            if (cursorPosY < lines.length - 1) {
+                var temp = lines[cursorPosY + 1];
+                lines[cursorPosY + 1] = lines[cursorPosY];
+                lines[cursorPosY] = temp;
+                moveCursorY(1);
+                modified = true;
+            }
+        }
+        if (key.name == 'e') {
+            exit();
+        }
+
     }
 
     // Normal Keys
@@ -136,7 +173,7 @@ function keyPressed(key) {
 function typeChar(char) {
     lineStr = lines[cursorPosY];
     lines[cursorPosY] = lineStr.slice(0, cursorPosX) + char + lineStr.slice(cursorPosX);
-    moveCursorX(-1);
+    moveCursorX(1);
     modified = true;
 }
 
@@ -144,7 +181,7 @@ function returnKey() {
     var newStr = lines[cursorPosY].slice(cursorPosX);
     lines[cursorPosY] = lines[cursorPosY].slice(0, cursorPosX)
     lines.splice(cursorPosY + 1, 0, newStr);
-    moveCursorX(-1);
+    moveCursorX(1);
     modified = true;
 }
 
@@ -153,20 +190,20 @@ function deleteKey() {
         if (cursorPosY > 0) {
             let originalLineLength = lines[cursorPosY].length;
             lines[cursorPosY - 1] += lines[cursorPosY];
-            moveCursorX(1);
+            moveCursorX(-1);
             moveCursorX(originalLineLength);
             lines.splice(cursorPosY + 1, 1);
             modified = true;
         }
     } else {
         lines[cursorPosY] = lines[cursorPosY].slice(0, cursorPosX - 1) + lines[cursorPosY].slice(cursorPosX);
-        moveCursorX(1);
+        moveCursorX(-1);
         modified = true;
     }
 }
 
 function moveCursorX(dir) {
-    cursorPosX -= dir;
+    cursorPosX += dir;
     if (cursorPosX < 0) {
         if (cursorPosY > 0) {
             cursorPosY--;
@@ -257,7 +294,7 @@ function renderLine(line, y) {
                             if (keywords.indexOf(str) != -1) {
                                 isKeyword = true;
                                 break;
-                            } else if (!isNaN(str) || str == 'true' || str == 'false') {
+                            } else if (!isNaN(str) || str == 'true' || str == 'false' || str == 'undefined' || str == "null") {
                                 isNumber = true;
                             }
                             i++;
@@ -297,10 +334,21 @@ function saveFile() {
     fs.writeFile(filename, string, (err) => {
         if (err) throw err;
         modified = false;
-        notSavedWarning = false;
         engine.render();
     });
 }
+
+function exit() {
+    if (!modified || notSavedWarning) {
+        engine.exit();
+    } else {
+        // User has not saved. Create popup.
+        notSavedWarning = true;
+        notification.notify("File is not saved. Changes will be lost. Press ESC to continue", 2200, 'red');
+        setTimeout(() => { notSavedWarning = false; }, 2200);
+    }
+}
+
 
 engine.init(setup, draw, keyPressed, 1000, true);
 
---
+++ notification.js
@@ -0,0 +1,31 @@
+module.exports.Notification = function (e) {
+    this.on = false;
+    this.message = "";
+    this.color = 'yellow'
+    this.timeOut = undefined;
+    this.engine = e;
+
+
+    this.notify = function (str, time, color) {
+        this.color = color || 'yellow';
+        this.on = true;
+        this.message = ' ' + str + ' ';
+        if (this.timeOut != undefined) clearTimeout(this.timeOut);
+        this.timeOut = setTimeout(() => { this.hide(this.engine); }, time);
+    }
+
+    this.show = function () {
+        if (this.on) {
+            this.engine.fillBackground(this.color);
+            this.engine.fillForeground('black');
+            this.engine.drawText(Math.floor(this.engine.width / 2) - Math.floor(this.message.length / 2), this.engine.height - 1, this.message);
+        }
+    }
+
+    this.hide = function (e) {
+        this.on = false;
+        this.timeOut = undefined;
+        e.render();
+    }
+
+}
